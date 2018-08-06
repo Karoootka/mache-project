57a56d2dcaaca0bd62fa9eed79b63dc49cdba30e
Karolina Madej <kkarolinamadej@gmail.com>
2018-08-06 18:04:11 +0200

    Add simple version of hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-function orderMacbookView() {
-    var viewportWidth = $(window).width();
-    if (viewportWidth <= 991) {
-      $('#macbook').addClass('order-first');
-    }
-    if (viewportWidth > 991) {
-      $('#macbook').removeClass('order-first');
-    }
-}
+// function orderMacbookView() {
+//     var viewportWidth = $(window).width();
+//     if (viewportWidth <= 991) {
+//       $('#macbook').addClass('order-first');
+//     }
+//     if (viewportWidth > 991) {
+//       $('#macbook').removeClass('order-first');
+//     }
+// }
+//
+// $(window).on('load', orderMacbookView);
+// $(window).resize(orderMacbookView);
 
-$(window).on('load', orderMacbookView);
-$(window).resize(orderMacbookView);
+// problem między 1008 a 992 -> .order-first { order: -1}
